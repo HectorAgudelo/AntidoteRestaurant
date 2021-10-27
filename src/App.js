4821d5e51c0c3b62adb1a695c3f90a2440ee6afc
Hector Agudelo <haacny86@gmail.com>
2021-10-27 17:37:22 -0400

    feat: added pictures, components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { NavigationBar } from './components/NavBar/NavigationBar';
+import { Gallery } from './components/Gallery/Gallery';
+import { LandingPage } from './components/LandingPage/LandingPage';
+import {BasicContact} from './components/BasicContact/BasicContact';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './style.css';
 
 function App() {
   return (
     <Router>
-      <>
+      <div className='App'>
         <NavigationBar />
-      </>
+        <LandingPage/>
+        <BasicContact />
+      </div>
     </Router>
   );
 }
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Container, Row, Image, Col } from 'react-bootstrap';
+import img4 from '../../image/Resized_20170311_172623.jpg';
+import { Logo } from '../Icons/Logo';
+import background from '../../image/circlePlants.jpg';
+import './style.css';
+
+export const LandingPage = () => {
+  return (
+    <Container
+      className='img'
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 'auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+      }}
+    >
+      <Container
+        style={{
+          display: 'flex',
+          marginBottom: '5%',
+          marginTop: '5%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Logo
+          color=''
+          height='150pt'
+          width='150pt'
+          viewBox='0 0 1023.000000 1024.000000'
+        />
+      </Container>
+      <Container
+        style={{
+          display: 'flex',
+          marginBottom: '5%',
+          marginTop: '5%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p className='text-break'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of
+          the printing and typesetting industry. Lorem Ipsum has been the
+          industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it to make a type specimen
+          book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was
+          popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem
+          Ipsum. Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is
+          simply dummy text of the printing and typesetting industry. Lorem
+          Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the
+          printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book. It has
+          survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in
+          the 1960s with the release of Letraset sheets containing Lorem Ipsum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </Container>
+    </Container>
+  );
+};
